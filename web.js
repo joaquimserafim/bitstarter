5cc68bea484c59e67b2dd00f929820918ad720ce
Joaquim Serafim <joaquim.serafim@gmail.com>
2013-07-08 22:08:26 +0000

    part1

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs  = require('fs');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var file = fs.readFileSync('index.html');
+  var buffer = new Buffer(file, 'utf-8');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log('Listening on '.concat(port));
 });
